ffa971e4b80c9cc0b21ed38143626a2cbc242f65
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-05-18 10:30:01 -0400

    created pwa asset was able to get the manifest to generate files in the public folder

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,24 +7,36 @@
    base: "./",
    plugins: [
      react(),
-     VitePWA({
-       manifest: {
-         name: "React-vite-app",
-         short_name: "react-vite-app",
-         description: "I am a simple vite app",
-         icons: [
-           {
-             src: '/android-icon-192x192.png',
-             sizes: '192x192',
-             type: 'image/png',
-             purpose: 'favicon'
-           },
-           {
-             src: '/placeholder-512.png',
-             sizes: '512x512',
-             type: 'image/png',
-             purpose: 'favicon'
-           }
+     VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Vite PWA Project',
+        short_name: 'Vite PWA Project',
+        theme_color: '#ffffff',
+        icons: [
+            {
+                src: 'pwa-64x64.png',
+                sizes: '64x64',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-192x192.png',
+                sizes: '192x192',
+                type: 'image/png'
+            },
+            {
+                src: 'pwa-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'any'
+            },
+            {
+                src: 'maskable-icon-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            }
          ],
          theme_color: '#171717',
          background_color: '#f0e7db',
