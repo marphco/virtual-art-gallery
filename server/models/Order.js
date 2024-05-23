e68ca4acebf1658e3d1197f3aa7c2eef00cb1651
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-05-23 19:54:45 -0400

    fixed the stripe to show the title of the product being bought

--- server/models/Order.js
+++ server/models/Order.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const cartItemSchema = new Schema({
   id: { type: String, required: true },
+  name: {type: String, required: true },
   price: { type: Number, required: true },
   quantity: { type: Number, required: true },
 });
