bb59fa56d2e7cdffb107958fc311a28336e7b251
Levangul <150108077+Levangul@users.noreply.github.com>
2024-05-27 13:39:13 -0400

    adapted backend for better user experience

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,9 @@
 const typeDefs = `#graphql
   type User {
     _id: ID
-    username: String
-    email: String
-    password: String
+    username: String!
+    email: String!
+    password: String!
     artCount: Int
     savedArt: [Art]
     orders: [Order]
