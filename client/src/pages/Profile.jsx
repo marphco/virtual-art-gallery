e92fb44900074f0feb79c83a348af8d733ae1f37
Levangul <150108077+Levangul@users.noreply.github.com>
2024-05-29 21:08:35 -0400

    added add to cart section on modals testing api endpoint

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,11 +3,11 @@ import { Form, Button, Container } from "react-bootstrap";
 import { useQuery, useMutation } from "@apollo/client";
 import { GET_USER_DATA } from "../utils/queries";
 import { REMOVE_ART, ADD_COMMENT, UPDATE_USERNAME } from "../utils/mutations";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import Zoom from 'react-medium-image-zoom';
-import Comments from '../components/Comments'
-import OrderHistory from '../components/OrderHistory';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import Zoom from "react-medium-image-zoom";
+import Comments from "../components/Comments";
+import OrderHistory from "../components/OrderHistory";
 
 const Profile = () => {
   const { loading, error, data, refetch } = useQuery(GET_USER_DATA);
@@ -20,9 +20,12 @@ const Profile = () => {
     refetchQueries: [{ query: GET_USER_DATA }],
   });
 
-  const [updateUsername, { error: updateUsernameError }] = useMutation(UPDATE_USERNAME, {
-    refetchQueries: [{ query: GET_USER_DATA }],
-  });
+  const [updateUsername, { error: updateUsernameError }] = useMutation(
+    UPDATE_USERNAME,
+    {
+      refetchQueries: [{ query: GET_USER_DATA }],
+    }
+  );
 
   const [commentTexts, setCommentTexts] = useState({});
   const [activeTab, setActiveTab] = useState("favorites");
@@ -62,7 +65,12 @@ const Profile = () => {
 
       refetch();
     } catch (error) {
-      console.error("Error adding comment:", error.message, error.networkError, error.graphQLErrors);
+      console.error(
+        "Error adding comment:",
+        error.message,
+        error.networkError,
+        error.graphQLErrors
+      );
     }
   };
 
@@ -79,17 +87,21 @@ const Profile = () => {
     }
   };
 
-  
-
   const renderFavorites = () => (
     <>
-      <Container id="your-favorites" className="flex justify-center flex-col mt-8">
+      <Container
+        id="your-favorites"
+        className="flex justify-center flex-col mt-8"
+      >
         <h2 className="text-3xl font-bold text-center">Your Favorites</h2>
         <p className="text-lg mb-6 text-center p-6">
-          Discover the masterpieces you've favorited! This section showcases the artworks you've loved the most. Enjoy a personalized collection of your favorite pieces, complete with artist details and your personal comments.
+          Discover the masterpieces you've favorited! This section showcases the
+          artworks you've loved the most. Enjoy a personalized collection of
+          your favorite pieces, complete with artist details and your personal
+          comments.
         </p>
       </Container>
-      
+
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full mt-4">
         {favorites.map((art) => (
           <li
@@ -119,10 +131,12 @@ const Profile = () => {
               <h3 className="art-title font-bold mb-2 text-center">
                 {art.title}
               </h3>
-              <p className="text-gray-600 mb-4 text-center">{art.description}</p>
-              <Comments artId={art.id}/>
-               <div>
-                 {art.comments && art.comments.length > 0 && (
+              <p className="text-gray-600 mb-4 text-center">
+                {art.description}
+              </p>
+              <Comments artId={art.id} />
+              <div>
+                {art.comments && art.comments.length > 0 && (
                   <ul className="mb-4">
                     {art.comments.map((comment) => (
                       <li key={comment.id} className="text-gray-600">
@@ -158,32 +172,36 @@ const Profile = () => {
       </ul>
     </>
   );
-  
+
   return (
     <div className="container mx-auto px-4 pt-44 pb-8 flex flex-col items-center bg-light-green">
       <h2 className="text-3xl font-bold mb-8 text-center">Your Profile</h2>
-      <p className="text-center py-4 mb-4 text-xl">Hello, <strong>{username}</strong>!</p>
+      <p className="text-center py-4 mb-4 text-xl">
+        Hello, <strong>{username}</strong>!
+      </p>
 
       {showUpdateField && (
         <div className="mb-8 w-full max-w-md mx-auto">
-          <h3 className="text-xl text-center font-semibold mb-4">Edit Username</h3>
+          <h3 className="text-xl text-center font-semibold mb-4">
+            Edit Username
+          </h3>
           <div className="flex justify-center items-center mb-3">
-          <input
-            type="text"
-            value={newUsername}
-            onChange={(e) => setNewUsername(e.target.value)}
-            placeholder="Search for art prints"
-            className="search-bar flex p-2 border border-gray-300 rounded-l-full"
-          />
-          <button
-            onClick={handleUpdateUsername}
-            type="submit"
-            className="save-btn py-2 px-6 text-white rounded-r-full"
-          >
-            Save
-          </button>
-            
-{/*             
+            <input
+              type="text"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+              placeholder="Search for art prints"
+              className="search-bar flex p-2 border border-gray-300 rounded-l-full"
+            />
+            <button
+              onClick={handleUpdateUsername}
+              type="submit"
+              className="save-btn py-2 px-6 text-white rounded-r-full"
+            >
+              Save
+            </button>
+
+            {/*             
             <input
               type="text"
               placeholder="Type Here"
@@ -199,7 +217,9 @@ const Profile = () => {
             </button> */}
           </div>
           {updateUsernameError && (
-            <p className="text-red-500 mt-2 ml-2">{updateUsernameError.message}</p>
+            <p className="text-red-500 mt-2 ml-2">
+              {updateUsernameError.message}
+            </p>
           )}
         </div>
       )}
@@ -212,22 +232,30 @@ const Profile = () => {
           Update Username
         </button>
       </div>
-      
+
       <div className="mb-8 flex flex-wrap justify-center">
         <button
           onClick={() => setActiveTab("favorites")}
-          className={`mx-2 px-4 py-2 ${activeTab === "favorites" ? "fav-btn text-white" : "bg-gray-200 text-gray-800 rounded-full"} mb-2`}
+          className={`mx-2 px-4 py-2 ${
+            activeTab === "favorites"
+              ? "fav-btn text-white"
+              : "bg-gray-200 text-gray-800 rounded-full"
+          } mb-2`}
         >
           Favorites
         </button>
         <button
           onClick={() => setActiveTab("order-history")}
-          className={`mx-2 px-4 py-2 ${activeTab === "order-history" ? "order-history-btn text-white" : "bg-gray-200 text-gray-800 rounded-full"} mb-2`}
+          className={`mx-2 px-4 py-2 ${
+            activeTab === "order-history"
+              ? "order-history-btn text-white"
+              : "bg-gray-200 text-gray-800 rounded-full"
+          } mb-2`}
         >
           Order History
         </button>
       </div>
-      
+
       {activeTab === "favorites" && renderFavorites()}
       {activeTab === "order-history" && <OrderHistory />}
     </div>
