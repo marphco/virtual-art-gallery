3e488d250a289ac659655927eda68c93c3419413
Jetnik Syla <sjetnik@gmail.com>
2024-06-10 10:29:49 -0400

    Fix issue with sender's name and email not being added to email notifications

--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -5,28 +5,28 @@ import "swiper/swiper-bundle.css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
 import logo from "../assets/logo.svg";
 import met from "../assets/met.png";
 import uffizi from "../assets/uffizi.png";
 import artic from "../assets/artic.png";
-import Arnaud from '../assets/arnaud.png';
-import ArtCenterTokyo from '../assets/art-center-tokyo.png';
-import CentroCultural from '../assets/centro-cultural.png';
-import Lacroix from '../assets/lacroix.png';
-import Lam from '../assets/lam.png';
-import Louvre from '../assets/louvre.png';
-import NationalGallery from '../assets/national-gallery.png';
-import Randall from '../assets/randall.png';
-import ScienceMuseum from '../assets/science-museum.png';
+import Arnaud from "../assets/arnaud.png";
+import ArtCenterTokyo from "../assets/art-center-tokyo.png";
+import CentroCultural from "../assets/centro-cultural.png";
+import Lacroix from "../assets/lacroix.png";
+import Lam from "../assets/lam.png";
+import Louvre from "../assets/louvre.png";
+import NationalGallery from "../assets/national-gallery.png";
+import Randall from "../assets/randall.png";
+import ScienceMuseum from "../assets/science-museum.png";
 import check from "../assets/check.svg";
 import success from "../assets/success.svg";
 import Auth from "../utils/auth";
 import { useCart } from "../context/CartContext.jsx";
 import emailjs from "@emailjs/browser";
 import Navbar from "./Navbar";
-import Notification from "./Shop/Notification"; // Import Notification component
+import Notification from "./Shop/Notification";
 
 const Result = () => {
   return (
@@ -57,14 +57,34 @@ const Homepage = () => {
     { id: 2, src: met, title: "Metropolitan Museum of New York", locked: true },
     { id: 3, src: uffizi, title: "Gallerie degli Uffizi", locked: true },
     { id: 4, src: Arnaud, title: "Galeria Raquel Arnaud", locked: true },
-    { id: 5, src: CentroCultural, title: "Centro Cultural Rio De Janeiro", locked: true },
+    {
+      id: 5,
+      src: CentroCultural,
+      title: "Centro Cultural Rio De Janeiro",
+      locked: true,
+    },
     { id: 6, src: Lacroix, title: "The Lacroix Art and History", locked: true },
     { id: 7, src: Lam, title: "Lam Museum of Anthropology", locked: true },
     { id: 8, src: Louvre, title: "Musée du Louvre", locked: true },
-    { id: 9, src: NationalGallery, title: "National Gallery of Art", locked: true },
+    {
+      id: 9,
+      src: NationalGallery,
+      title: "National Gallery of Art",
+      locked: true,
+    },
     { id: 10, src: Randall, title: "Randall Museum of Science", locked: true },
-    { id: 11, src: ScienceMuseum, title: "International Science Museum", locked: true },
-    { id: 12, src: ArtCenterTokyo, title: "Tokyo National Art Center", locked: true },
+    {
+      id: 11,
+      src: ScienceMuseum,
+      title: "International Science Museum",
+      locked: true,
+    },
+    {
+      id: 12,
+      src: ArtCenterTokyo,
+      title: "Tokyo National Art Center",
+      locked: true,
+    },
   ];
 
   const subscriptionItems = [
@@ -181,14 +201,17 @@ const Homepage = () => {
   };
 
   const handleAddToCart = (item) => {
-    const imageUrl = item.imageUrl || logo; // Set imageUrl for subscription items
+    const imageUrl = item.imageUrl || logo;
     addToCart({ ...item, imageUrl });
     setNotification({
       visible: true,
       message: `${item.title} has been added to cart!`,
       imageUrl,
     });
-    setTimeout(() => setNotification({ visible: false, message: "", imageUrl: "" }), 3000);
+    setTimeout(
+      () => setNotification({ visible: false, message: "", imageUrl: "" }),
+      3000
+    );
   };
 
   return (
@@ -199,15 +222,16 @@ const Homepage = () => {
         activeForm={activeForm}
         setActiveForm={setActiveForm}
       />
-
       <Row id="hero" className="flex justify-center">
         <Container id="logo">
           <img src={logo} alt="Panorama - Virtual Art Gallery" />
         </Container>
       </Row>
-
       <Row id="intro" className="flex justify-center">
-        <Container id="text-intro" className="d-flex justify-center text-center">
+        <Container
+          id="text-intro"
+          className="d-flex justify-center text-center"
+        >
           <h1 className="text-4xl p-5 font-bold">
             An Immersive
             <br />
@@ -227,7 +251,6 @@ const Homepage = () => {
           </p>
         </Container>
       </Row>
-
       <Row className="tranding flex justify-center">
         <Container>
           <h2 className="text-3xl font-bold mb-4 mt-8 text-center">
@@ -240,7 +263,10 @@ const Homepage = () => {
             will captivate your senses. Start your art journey today!
           </p>
 
-          <div className="swiper tranding-slider" key={isLoggedIn ? "loggedIn" : "loggedOut"}>
+          <div
+            className="swiper tranding-slider"
+            key={isLoggedIn ? "loggedIn" : "loggedOut"}
+          >
             <div className="swiper-wrapper">
               {galleries.map((gallery) => (
                 <div key={gallery.id} className="swiper-slide tranding-slide">
@@ -297,8 +323,10 @@ const Homepage = () => {
           </div>
         </Container>
       </Row>
-
-      <Container id="subscription-text" className="flex justify-center flex-col mt-8">
+      <Container
+        id="subscription-text"
+        className="flex justify-center flex-col mt-8"
+      >
         <h2 className="text-3xl font-bold text-center">
           Unlock Exclusive Benefits with Our Subscriptions
         </h2>
@@ -309,7 +337,6 @@ const Homepage = () => {
           enrich your art experience and stay connected!
         </p>
       </Container>
-
       <Row id="subscription-block" className="flex justify-center mt-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
           {subscriptionItems.map((item) => (
@@ -349,7 +376,6 @@ const Homepage = () => {
           ))}
         </div>
       </Row>
-
       <Container className="flex justify-center flex-col mt-40">
         <h2 className="text-3xl font-bold text-center">Get in Touch with Us</h2>
         <p className="text-lg mb-6 text-center p-6 pb-4">
@@ -357,7 +383,6 @@ const Homepage = () => {
           we'll respond promptly. We're here to help!
         </p>
       </Container>
-
       <Container>
         <Form
           ref={form}
@@ -368,7 +393,7 @@ const Homepage = () => {
             <Form.Control
               type="text"
               placeholder="Name"
-              name="from_name"
+              name="user_name"
               className="block w-full px-4 py-2 text-gray-800 bg-white border border-gray-300 rounded-md focus:ring-opacity-50"
               required
             />
@@ -378,7 +403,7 @@ const Homepage = () => {
             <Form.Control
               type="email"
               placeholder="name@example.com"
-              name="reply_to"
+              name="user_email"
               className="block w-full px-4 py-2 text-gray-800 bg-white border border-gray-300 rounded-md focus:ring-opacity-50"
               required
             />
@@ -405,8 +430,8 @@ const Homepage = () => {
           <Row className="success">{result ? <Result /> : null}</Row>
         </Form>
       </Container>
-
-      <Notification notification={notification} /> {/* Include Notification component */}
+      <Notification notification={notification} />{" "}
+      {/* Include Notification component */}
     </>
   );
 };
